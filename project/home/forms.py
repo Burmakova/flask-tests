ab91b90d74c40ca6e492f40bd09104a1ad4f78f1
Anastasia Burmakova <burmakova@tut.by>
2019-12-04 20:52:13 +0300

    updated pakages version

--- project/home/forms.py
+++ project/home/forms.py
@@ -1,9 +1,9 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 
-class MessageForm(Form):
+class MessageForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     description = TextAreaField(
         'Description', validators=[DataRequired(), Length(max=1400)])
